9622cafa191f8c1128e59c7b1f6a2fec8740ac38
Alldlights <emekachris202@gmail.com>
2022-03-15 10:08:22 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,12 +8,12 @@
  */
 int _islower(int c)
 {
-	c = _islower('H');
-	_putchar(c + '0');
-	c = _islower('o');
-	_putchar(r + '0');
-	c = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
