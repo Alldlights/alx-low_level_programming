0b774919a3be44eca971d9a8cadb141399e227c7
Alldlights <emekachris202@gmail.com>
2022-03-15 08:58:51 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: this program prints _putchar in a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char msg[9] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(msg[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
