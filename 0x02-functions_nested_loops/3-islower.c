3bdba3ebdd9f653f78aadf0984d4822070d3ccb8
Alldlights <emekachris202@gmail.com>
2022-03-15 10:01:28 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ * Return: 0
+ */
+int _islower(int c)
+{
+	c = _islower('H');
+	_putchar(c + '0');
+	c = _islower('o');
+	_putchar(r + '0');
+	c = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
