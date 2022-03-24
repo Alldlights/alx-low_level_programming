0a0d26ed7a31f28a3ddf9fb39ebf5b04b04618ac
Alldlights <emekachris202@gmail.com>
2022-03-24 12:56:23 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,7 +9,8 @@ void print_number(int n)
 {
 	if (n / 10 != 0)
 	{
-		print_number(n / 10);
+		_putchar(n % 10);
+		print_number((i - i % 10) / 10);
 		if (n > 0)
 		{
 			_putchar(n % 10 + '0');
@@ -19,7 +20,7 @@ void print_number(int n)
 			_putchar(-n % 10 + '0');
 		}
 	}
-	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n >= 0))
 	{
 		_putchar(n % 10 + '0');
 	}
