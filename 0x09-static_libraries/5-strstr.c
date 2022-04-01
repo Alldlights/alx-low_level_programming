44e017b6ae198e491990f72c9a46fe94e084d47a
Alldlights <emekachris202@gmail.com>
2022-04-01 16:26:21 -0700

    5-strstr.c

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - finds the first occurances of the substring needle
+ * in the string haystack
+ * @haystack: parameter for haystack
+ * @needle: parameter for needle
+ * Return: pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (needle);
+}
