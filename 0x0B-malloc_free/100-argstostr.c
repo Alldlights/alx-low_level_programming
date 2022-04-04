efe4a601d8bc0ee5c3c00b12d22f5fc69630ee59
Alldlights <emekachris202@gmail.com>
2022-04-04 10:15:30 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,7 +17,8 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 0; total = 0; i < ac; i++)
+	for (i = 0; i < ac; i++)
+		total = 0;
 	{
 		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
 			;
