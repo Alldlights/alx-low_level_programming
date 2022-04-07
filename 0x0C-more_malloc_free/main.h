4c1d09e450cacf9728213ece05356b187216b07a
Alldlights <emekachris202@gmail.com>
2022-04-07 05:22:33 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_FILE
 #define MAIN_FILE
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -12,5 +12,5 @@ int *array_range(int min, int max);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
-void *realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
