091f7b87f9559a7671cdb17c5d2e3cf4dc6030e5
Alldlights <emekachris202@gmail.com>
2022-04-21 16:45:23 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,10 @@
  * before_main - Print statement within the finction
  * before main function is executed.
  */
-void before_main(void)  __attribute__((constructor));
+void before_main(void)  __attribute__((constructor))
 
-void before_main(void)
+before_main(void)
 {
 	printf("You're best and yet, you must allow,\n");
-	printf("I bore my house upon myn back!\n");
+	printf("I bore my house upon my back!\n");
 }
