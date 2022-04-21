efd0991f5bcffbd4b82a9653456ebca4fe1ace76
Alldlights <emekachris202@gmail.com>
2022-04-21 16:41:44 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * before_main - Print statement within the finction
+ * before main function is executed.
+ */
+void before_main(void)  __attribute__((constructor));
+
+void before_main(void)
+{
+	printf("You're best and yet, you must allow,\n");
+	printf("I bore my house upon myn back!\n");
+}
