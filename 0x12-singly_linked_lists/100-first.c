039697be3f4f0e79d29bb7a68c01f709d47fc48c
Alldlights <emekachris202@gmail.com>
2022-04-21 15:22:53 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,7 @@
 /**
  * bmain - function executed before main
  * Return: Always 0
-*/
-
+ */
 void bmain(void) __attribute__ ((constructor));
 
 void bmain(void)
@@ -13,8 +12,12 @@ void bmain(void)
 	printf("I bore my house upon my back!\n");
 }
 
+/**
+ * main - prints a string in a new line
+ * Return: 0
+ */
 int main(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("(A tortise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
 }
