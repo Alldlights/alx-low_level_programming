2e20658b2b7d16e47d35903b8b9b871181d8e13c
Alldlights <emekachris202@gmail.com>
2022-04-21 17:05:04 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,3 +10,15 @@ void before_main(void)
 	printf("You're best and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
+
+/**
+ * main - checks code 
+ *
+ * Returns: 0;
+ */
+int main (void)
+{
+	printf("(A tortise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+
+	return (0);
+}
