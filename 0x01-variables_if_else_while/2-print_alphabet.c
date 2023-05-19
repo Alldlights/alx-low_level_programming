6d409370f42ceb76fbbcb547f7e8111e84093cd7
Alldlights <emekachris202@gmail.com>
2023-05-19 09:30:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints alphabets in lowercase in a new line
+ *
+ * Retrun: Always 0 on Success
+ */
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+	while (start <= end)
+	{
+		putchar(start);
+		start++;
+	}
+	putchar('\n');
+
+	return (0);
+}
