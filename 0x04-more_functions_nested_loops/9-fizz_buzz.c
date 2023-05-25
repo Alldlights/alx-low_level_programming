c7a720c95329cc0de3a0d4eb4431d7069c954b11
Alldlights <emekachris202@gmail.com>
2023-05-25 10:16:01 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program prints numbers from 1 tp 100. if multiples of 3, print
+ * the string "Fizz", else if multiple of 5 print the string "Buzz" otherwise
+ * print "FizzBuzz" if multiple of 3 and 5.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 1;
+
+	while (a <= 100)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
