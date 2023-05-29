2cc2c07ffe10254a614c8f0c8090774ab1017716
Alldlights <emekachris202@gmail.com>
2023-05-29 12:56:40 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every character of a string from the first
+ * character. use putchar() to print even numbers from the string.
+ * @str: pointer to the string variable
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
