eb495f4093a48bb7a2436cfbb9e7824885c10556
Alldlights <emekachris202@gmail.com>
2023-06-01 02:53:10 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,34 +1,27 @@
 #include "main.h"
 
 /**
- * leet - funtion that encodes a string into 1337
- * @s: string to be encoded
- * Description: use only 1 if statement, use 2 loops, don't use the switch
- * statement and ternary operation. Aa, Ee, Oo, Tt and Ll, should be replaced
- * by 4,3,0,7 and 1.
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded
  *
- * Return: encoded string
+ * Return: A pointer to the encoded string
  */
-
-char *leet(char *s)
+char *leet(char *str)
 {
-	int a = 0, l = 5, i;
-	char r[5] = {'A', 'E', 'O', 'T', 'L'};
-	char n[5] = {'4', '3', '0', '7', '1'};
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (s[a])
+	while (str[indx1])
 	{
-		i = 0;
-
-		while (i < l)
+		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
-			if (s[a] == r[i] || s[a] - 32 == r[i])
-			{
-				s[a] = n[i];
-			}
-			i++;
+			if (str[indx1] == leet[indx2] ||
+				str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
 		}
-		a++;
+
+		indx1++;
 	}
-	return (s);
+
+	return (str);
 }
