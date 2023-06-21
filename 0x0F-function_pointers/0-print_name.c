3be37fefb35fa2457e6ffd9451dc2d1d2e06e988
Alldlights <emekachris202@gmail.com>
2023-06-21 06:58:52 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name string
+ * @f: pointer to function that takes the neame argument
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && *name != '\0' && f != NULL)
+		f(name);
+}
