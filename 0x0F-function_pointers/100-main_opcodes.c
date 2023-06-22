d09713fe3c780aa09d88a42820a2fe9718dd7d65
Alldlights <emekachris202@gmail.com>
2023-06-22 05:45:01 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: A program that prints the opcodes of its own main function
+ *
+ * Return: 0on success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < atoi(argv[1]) - 1; i++)
+		printf("%02hhx ", ((char *)main)[i]);
+	printf("%02hhx\n", ((char *)main)[i]);
+	return (0);
+}
+
