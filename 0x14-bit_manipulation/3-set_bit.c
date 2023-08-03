4e2a5065c9d568616f36f92a1f30d678cb97cdae
Alldlights <emekachris202@gmail.com>
2023-08-03 14:36:09 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,10 +11,11 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int mask;
 
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
 	mask = 1UL << index;
 	*n |= mask;
 
-	if (index >= sizeof(unsigned long int) * 8)
-		return (-1);
 	return (1);
 }
