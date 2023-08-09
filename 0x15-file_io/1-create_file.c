e07b02d3a3ba2825cc3e1f8dc56419e0573c3977
Alldlights <emekachris202@gmail.com>
2023-08-08 20:42:44 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,26 +10,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes_written;
+	int fd, bytes_written, bytes_read = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		bytes_written = write(fd, text_content, sizeof(text_content));
-		if (bytes_written == -1)
-		{
-			close(fd);
+		while (text_content[bytes_read])
+			bytes_read++;
+		bytes_written = write(fd, text_content, bytes_read);
+		if (bytes_written != bytes_read)
 			return (-1);
-		}
 	}
+
 	close(fd);
+
 	return (1);
 }
 
