fc0c8c0a2186e9e00f1b974c0731c690b61253e0
Alldlights <emekachris202@gmail.com>
2023-08-08 22:51:57 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((bytes_read = read(fd_from, buff, BUFSIZ)) > 0)
 	{
@@ -55,4 +54,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-	
