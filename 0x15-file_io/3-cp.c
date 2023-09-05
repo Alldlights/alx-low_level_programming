b3d96e9a462a45a2aabc90fa2d1cb8963a4dfecc
Alldlights <emekachris202@gmail.com>
2023-09-04 18:22:47 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,18 +9,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_src, fd_dest;
-	ssize_t read_bytes;
+	int fd_src, fd_dest, read_bytes;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-
+	}
 	fd_src = open(argv[1], O_RDONLY);
 	if (fd_src < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
 	fd_dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	while ((read_bytes = read(fd_src, buf, BUFSIZ)) > 0)
 	{
@@ -32,15 +34,20 @@ int main(int argc, char *argv[])
 		}
 	}
 	if (read_bytes < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[1]);
 		exit(98);
-
+	}
 	if (close(fd_src) < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_src);
 		exit(100);
+	}
 	if (close(fd_dest) < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest);
 		exit(100);
+	}
 	return (0);
 }
 
