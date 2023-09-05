644cca80a5fa182f70858d083ab20c92d3161187
Alldlights <emekachris202@gmail.com>
2023-09-04 18:25:56 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - Program that copies the content of one file to another
- * @argc: argument count
- * @argv: argument vector
+ * main - copies the content of a file to another file
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
  *
- * Return: 0;
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int fd_src, fd_dest, read_bytes;
+	int fd_r, fd_w, r, a, b;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
@@ -17,37 +17,36 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_src = open(argv[1], O_RDONLY);
-	if (fd_src < 0)
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	while ((read_bytes = read(fd_src, buf, BUFSIZ)) > 0)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		if (fd_dest < 0 || write(fd_dest, buf, read_bytes) != read_bytes)
+		if (fd_w < 0 || write(fd_w, buf, r) != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_src);
+			close(fd_r);
 			exit(99);
 		}
 	}
-	if (read_bytes < 0)
+	if (r < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(fd_src) < 0)
+	a = close(fd_r);
+	b = close(fd_w);
+	if (a < 0 || b < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_src);
-		exit(100);
-	}
-	if (close(fd_dest) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest);
+		if (a < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+		if (b < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
 	return (0);
 }
-
