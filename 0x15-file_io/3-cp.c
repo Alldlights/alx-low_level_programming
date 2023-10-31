9f1e01f7f30e46136f130a17ef96565846bf674e
Alldlights <emekachris202@gmail.com>
2023-10-31 14:24:44 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,25 +17,22 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((bytes_read = read(fd_r, buf, BUFSIZ)) > 0)
 	{
 		if (fd_w < 0 || write(fd_w, buf, bytes_read) != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: CAn't write to %s\n", argv[2]);
 			close(fd_r);
 			exit(99);
 		}
 	}
-
 	if (bytes_read < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
