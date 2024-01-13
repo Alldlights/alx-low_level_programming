17012dca695ab140d13014f2ef3a01221d638a5c
Alldlights <emekachris202@gmail.com>
2024-01-13 14:40:06 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: on success 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n" argv[0]);
+
+	return (0);
+}
