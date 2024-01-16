195d0d330d84ef14e51183b7fa280a1e5fc30410
Alldlights <emekachris202@gmail.com>
2024-01-16 01:00:46 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,10 +12,10 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	size_t length, i, j;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	length = strlen(s1) + strlen(s2);
 	ptr = malloc(sizeof(char) * (length + 1));
 	if (ptr == NULL)
