bf59ca810acc57044a21945f39f0882a43350610
Alldlights <emekachris202@gmail.com>
2024-01-23 16:40:30 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure of type dog
+ * @name: Name of the dog
+ * @owner: The owner of the dog
+ * @age: The age of teh dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /* DOG_H */
