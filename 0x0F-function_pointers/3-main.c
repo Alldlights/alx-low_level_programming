8e37f4176690dfbf4d51def963d2d2181f78d925
Alldlights <emekachris202@gmail.com>
2024-01-26 03:02:45 -0800

    simple calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int);
+	int num1, num2, result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
