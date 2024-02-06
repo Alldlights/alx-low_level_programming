96b051a09ce37e5150dd9c7d031efcd76c0909a3
Alldlights <emekachris202@gmail.com>
2024-02-05 18:57:37 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * before_main - Function that prints before the main function
+ *
+ * Return: Nothing
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
