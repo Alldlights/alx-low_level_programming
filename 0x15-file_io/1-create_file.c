123c1d4281b7c4e62551576fd60b183b1bd89aee
Alldlights <emekachris202@gmail.com>
2024-02-18 13:08:18 -0800

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * create_file - Function that creates a file
+ * @filename: Name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, else -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t write_byt;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_RDWR | O_CREAT, 0600);
+	if (file == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		write_byt = write(file, text_content, _strlen(text_content));
+		if (write_byt == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
+}
+
+/**
+ * _strlen - Function the returns the lenght of a string
+ * @str: NULL terminated string
+ * Return: Lenght of the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
 #endif /* MAIN_H */
